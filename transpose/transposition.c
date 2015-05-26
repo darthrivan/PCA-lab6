da4239cd1af09ba88ebe596e139e232e4afb68d0
Víctor Mas <victor.mas@est.fib.upc.edu>
2015-05-26 20:04:15 +0200

    Eklund matrix transposition algorithm

--- transpose/transposition.c
+++ transpose/transposition.c
@@ -6,11 +6,37 @@
 
 void transpose(float *dst, float *src, int dim) {
     int i, j, ii, jj;
+    float aux;
     for (i = 0; i < dim; i += BLOCK_SIZE) {
-        for(j = 0; j < dim; j += BLOCK_SIZE) {
+        // Transposicion de la diagonal
+        for (ii = i; ii < i+BLOCK_SIZE; ++ii) {
+            for (jj = i; jj < i+BLOCK_SIZE; ++jj) {
+                dst[jj * dim + ii] = src[ii * dim + jj];
+            }
+        }
+        for(j = i+BLOCK_SIZE; j < dim; j += BLOCK_SIZE) {
+            // Transposicion de no diagonal
+            for (ii = i; ii < i+BLOCK_SIZE; ++ii) {
+                for (jj = ii+1; jj < j+BLOCK_SIZE; ++jj) {
+                    // TODO arreglar accesos
+                    aux = src[ii * dim + jj];
+                    // src[ii * dim + jj] = src[jj * dim + ii];
+                    src[ii * dim + jj] = src[(ii+jj-j) * dim + (ii-i)];
+                    src[jj * dim + ii] = aux;
+                }
+            }
+            for (ii = j; ii < j+BLOCK_SIZE; ++ii) {
+                for (jj = ii+1; jj < i+BLOCK_SIZE; ++jj) {
+                    aux = src[ii * dim + jj];
+                    // src[ii * dim + jj] = src[jj * dim + ii];
+                    src[ii * dim + jj] = src[(ii+jj-j) * dim + (ii-i)];
+                    src[jj * dim + ii] = aux;
+                }
+            }
             for (ii = i; ii < i+BLOCK_SIZE; ++ii) {
                 for (jj = j; jj < j+BLOCK_SIZE; ++jj) {
                     dst[jj * dim + ii] = src[ii * dim + jj];
+                    dst[ii * dim + jj] = src[jj * dim + ii];
                 }
             }
         }
@@ -27,8 +53,8 @@ int main(int argc, char *argv[]) {
     
     int dim = atoi(argv[1]);
 
-    float *src = malloc(dim*dim*sizeof(int));
-    float *dst = malloc(dim*dim*sizeof(int));
+    float *src = malloc(dim*dim*sizeof(float));
+    float *dst = malloc(dim*dim*sizeof(float));
 
     srand(0);
     for (i = 0; i < dim*dim; ++i)
