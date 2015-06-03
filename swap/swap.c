fed8c7a8687291d8d0de3abf249bf80b95b66aae
DarthRivan <masanalafuentecarlos@gmail.com>
2015-06-03 17:46:14 +0200

    Swap vectorized

--- swap/swap.c
+++ swap/swap.c
@@ -1,21 +1,33 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
- 
+#include "emmintrin.h"
+#include "tmmintrin.h"
+
+#define BUFFER_SIZE (1<<20)
+
 int main(int argc, char *argv[])
 {
-    char c1, c2;
-    int n1, n2;
+    __m128i a, b;
+    int n1, n2, i;
+    char buffer[BUFFER_SIZE];
  
-    while (((n1=read(0, &c1, 1)) > 0) && ((n2=read(0, &c2, 1)) > 0))
+    b = _mm_set_epi8(14,15,12,13,10,11,8,9,6,7,4,5,2,3,0,1);
+    while ((n1=read(0, buffer, BUFFER_SIZE*sizeof(char))) > 0)
     {
-        write(1, &c2, 1);
-        write(1, &c1, 1);
+		for (i = 0; i < 16*(n1/16); i += 16) {
+			a = _mm_load_si128((__m128i *) &buffer[i]);
+			a = _mm_shuffle_epi8(a, b);
+			_mm_store_si128((__m128i *) &buffer[i], a);
+		}
+		// epilogo
+		for (; i < n1-1; i += 2) {
+			buffer[i]   = buffer[i] ^ buffer[i+1];
+			buffer[i+1] = buffer[i] ^ buffer[i+1];
+			buffer[i]   = buffer[i] ^ buffer[i+1];
+		}
+		write(1, buffer, n1);
     }
- 
-    if ((n1==1) && (n2==0)) write(1, &c1, 1);
  
     return 0;
 }
